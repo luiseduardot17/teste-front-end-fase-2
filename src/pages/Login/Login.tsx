6c46d9aa18fab19862114660e5d4393bcddf4f31
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-07 20:20:33 -0300

    ✨ feat: criado formulario de login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import bg from '../../assets/images/bg-login.png'
+import logo from '../../assets/images/telemetrixLogo.png'
+import S from './Login.module.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="container-fluid p-0">
+      <section className={S.section}>
+        <img src={bg} className="img-fluid" alt="pessoas trabalhando" />
+        <div className={S.formulario}>
+          <img src={logo} className="img-fluid w-50 pb-5" id="image" alt="logo telemetrix" />
+          <form className="mb-3 d-flex flex-column w-50">
+            <div className="form-floating mb-3 w-100">
+              <input type="email" className="form-control border-primary border-0 border-bottom rounded-0" id="floatingInput" placeholder="Email"
+                required />
+              <label htmlFor="floatingInput m-0">Email</label>
+            </div>
+            <div className="form-floating mb-3 w-100">
+              <input type="password" className="form-control border-primary border-0 border-bottom rounded-0" id="floatingInput" placeholder="Senha"
+                required />
+              <label htmlFor="floatingInput">Digite Sua Senha</label>
+            </div>
+            <button type="submit" className="btn btn-primary w-100 mx-auto">Entrar</button>
+          </form>
+        </div>
+      </section>
+    </div>
   )
 }
 
