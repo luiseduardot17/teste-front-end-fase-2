b2fbba765d5224a2de39833a4937e2aa95871e0e
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-08 15:42:54 -0300

    ✨ feat: estilização dos icones

---
+++ src/styles/styledIcons.ts
@@ -0,0 +1,14 @@
+import { FaRegPlusSquare, FaSistrix } from "react-icons/fa";
+import styled from "styled-components"
+
+export const FindIcon = styled(FaSistrix)`
+    width: 25px;
+    height: 25px;
+    color:  #005187;
+`;
+
+export const AddIcon = styled(FaRegPlusSquare)`
+    width: 25px;
+    height: 25px;
+    color:  #005187;
+`;
