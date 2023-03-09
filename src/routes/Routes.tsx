79bcf87b998d15766cff745da14b03baba0eca74
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-08 22:26:18 -0300

    ✨ feat: add novas rotas

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Route } from 'react-router'
 import { BrowserRouter, Routes as Switch } from 'react-router-dom'
+import BaseCadastros from '../pages/Cadastros/BaseCadastros'
+import Clientes from '../pages/Cadastros/Clientes/Clientes'
+import Fornecedores from '../pages/Cadastros/Fornecedores/Fornecedores'
+import Usuarios from '../pages/Cadastros/Usuarios/Usuarios'
 import Home from '../pages/Home/Home'
 import Login from '../pages/Login/Login'
 import LoginCliente from '../pages/Login/LoginCliente/LoginCliente'
@@ -9,9 +13,14 @@ const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/login' element={<Login/>}/>
-                <Route path='/login/cliente' element={<LoginCliente/>}/>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/login/cliente' element={<LoginCliente />} />
+                <Route path='/cadastros' element={<BaseCadastros />}>
+                    <Route path='usuarios' element={<Usuarios />} />
+                    <Route path='clientes' element={<Clientes/>} />
+                    <Route path='fornecedores' element={<Fornecedores/>} />
+                </Route>
             </Switch>
         </BrowserRouter>
     )
