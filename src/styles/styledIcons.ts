8938f81016074667e3ba73c2b95fccc8242fb1a7
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-08 22:25:53 -0300

    💄 feat: add novas estilizações para icones

--- src/styles/styledIcons.ts
+++ src/styles/styledIcons.ts
@@ -1,4 +1,6 @@
-import { FaRegPlusSquare, FaSistrix } from "react-icons/fa";
+import { FaRegPlusSquare, FaSistrix} from "react-icons/fa";
+import { VscTrash } from "react-icons/vsc"
+import { AiFillCaretRight } from "react-icons/ai"
 import styled from "styled-components"
 
 export const FindIcon = styled(FaSistrix)`
@@ -11,4 +13,31 @@ export const AddIcon = styled(FaRegPlusSquare)`
     width: 25px;
     height: 25px;
     color:  #005187;
+`;
+
+export const RightIcon = styled(AiFillCaretRight)`
+    width: 30px;
+    height: 30px;
+    color:  #005187;
+`;
+
+export const DelIcon = styled(VscTrash)`
+    width: 25px;
+    height: 25px;
+    color:  #6C757D;
+    cursor: pointer;
+`;
+
+export const EditIcon = styled.img`
+    width: 20px;
+    height: 20px;
+    color:  #6C757D;
+    cursor: pointer;
+`;
+
+export const ConfIcon = styled.img`
+    width: 30px;
+    height: 30px;
+    color:  #6C757D;
+    cursor: pointer;
 `;
