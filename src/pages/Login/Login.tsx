9183a7cfd8515b40cd9e4c78030d507560a74f1a
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-09 13:34:47 -0300

    💄 style: estilização de classes

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import bg from '../../assets/images/bg-login.png'
+import bg from '../../assets/images/bg-login1.png'
 import logo from '../../assets/images/telemetrixLogo.png'
 import S from './Login.module.css'
 
@@ -7,21 +7,21 @@ const Login = () => {
   return (
     <div className="container-fluid p-0">
       <section className={S.section}>
-        <div className={S.banner}><img src={bg} className="img-fluid" alt="pessoas trabalhando" /></div>
+        <div className={S.banner}><img src={bg} className="img-fluid w-100" alt="pessoas trabalhando" /></div>
         <div className={S.formulario}>
           <img src={logo} className="img-fluid w-50 pb-5" id="image" alt="logo telemetrix" />
           <form className="mb-3 d-flex flex-column w-50">
             <div className="form-floating mb-3 w-100">
-              <input type="email" className="form-control border-primary border-0 border-bottom rounded-0" id="floatingInput" placeholder="Email"
+              <input type="email" className="form-control rounded-0" id="floatingInput1" placeholder="Email"
                 required />
-              <label htmlFor="floatingInput m-0">Email</label>
+              <label htmlFor="floatingInput1" className='text-secondary'>Email</label>
             </div>
             <div className="form-floating mb-3 w-100">
-              <input type="password" className="form-control border-primary border-0 border-bottom rounded-0" id="floatingInput" placeholder="Senha"
+              <input type="password" className="form-control rounded-0" id="floatingInput2" placeholder="Senha"
                 required />
-              <label htmlFor="floatingInput">Digite Sua Senha</label>
+              <label htmlFor="floatingInput2" className='text-secondary'>Digite Sua Senha</label>
             </div>
-            <button type="submit" className="btn btn-primary w-100 mx-auto">Entrar</button>
+            <button type="submit" className="btn btn-primary w-100 mx-auto border-0 p-2">Entrar</button>
           </form>
         </div>
       </section>
