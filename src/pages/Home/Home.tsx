24cab3ee5cc2464832e7fbbea21024c49584b573
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-08 22:27:10 -0300

    👌 style: formatação de código

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,9 @@
-import React from 'react'
-import Sidebar from '../../components/Sidebar/Sidebar'
 import S from './Home.module.css'
 
 const Home = () => {
   return (
     <div className={S.container}>
-      <Sidebar/>
-      <section className={S.section}>
-        
-      </section>
+      VOCÊ ESTA NA HOME
     </div>
   )
 }
