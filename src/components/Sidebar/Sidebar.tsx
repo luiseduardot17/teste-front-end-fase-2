7cb1078a37e8ab67e710dc0f0aa1ca7dcf84f001
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-09 22:44:23 -0300

    💄 style: pequeno ajuste no tamanho da sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,12 +5,12 @@ import { ConfIcon, RightIcon } from '../../styles/styledIcons';
 
 const Sidebar = () => {
     return (
-        <nav className="navbar bg-body border-3 border-bottom border-tertiary p-3 pb-4 mb-5">
+        <nav className="navbar bg-body border-3 border-bottom border-tertiary p-3 pb-2 mb-5">
             <div className="container-fluid">
                 <RightIcon type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" />
                 <div className='pe-5 me-5'><ConfIcon src={cog} /></div>
                 <div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" aria-expanded="false">
-                    <div className="offcanvas-header border-bottom border-tertiary pb-4 pe-0">
+                    <div className="offcanvas-header border-bottom border-tertiary pb-2 pe-0">
                         <div><img src={logo} className="img-fluid w-100 mx-auto" id="image" alt="logo telemetrix" /></div>
                         <div><RightIcon type="button" data-bs-dismiss="offcanvas" aria-label="Close" /></div>
                     </div>
