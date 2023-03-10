9b14bc012d923352b3643b7e5971414c7b476cad
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-09 21:38:30 -0300

    ✨ feat: add links para facilitar acesso a paginas

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom'
+import Sidebar from '../../components/Sidebar/Sidebar'
 import S from './Home.module.css'
 
 const Home = () => {
   return (
     <div className={S.container}>
-      VOCÊ ESTA NA HOME
+      <div className='container-fluid d-flex justify-content-evenly'>
+      <Link to='/login' className="nav-link text-light p-0">1° TELA DE LOGIN</Link>
+      <Link to='/login/cliente' className="nav-link text-light p-0">TELA DE LOGIN CLIENTES</Link>
+      <Link to='/cadastros/usuarios' className="nav-link text-light p-0">2° TELA DE CADASTRO DE USUÁRIOS</Link>
+      <Link to='/cadastros/grupos' className="nav-link text-light p-0">3° TELA DE CADASTRO DE GRUPOS</Link>
+    </div>
     </div>
   )
 }
