b2a716fd571a6ed3e78ae432319b522bbb250588
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-09 22:42:31 -0300

    💄 feat: feita mudança no icone de editar

--- src/styles/styledIcons.ts
+++ src/styles/styledIcons.ts
@@ -1,44 +1,45 @@
-import { FaRegPlusSquare, FaSistrix} from "react-icons/fa";
-import { VscTrash } from "react-icons/vsc"
-import { AiFillCaretRight } from "react-icons/ai"
-import styled from "styled-components"
+import { FaRegPlusSquare, FaSistrix } from "react-icons/fa";
+import { VscTrash } from "react-icons/vsc";
+import { AiFillCaretRight } from "react-icons/ai";
+import { TbFilePencil } from "react-icons/tb";
+import styled from "styled-components";
 
 export const FindIcon = styled(FaSistrix)`
-    width: 25px;
-    height: 25px;
-    color:  #005187;
+  width: 25px;
+  height: 25px;
+  color: #005187;
 `;
 
 export const AddIcon = styled(FaRegPlusSquare)`
-    width: 25px;
-    height: 25px;
-    color:  #005187;
-    cursor: pointer;
+  width: 25px;
+  height: 25px;
+  color: #005187;
+  cursor: pointer;
 `;
 
 export const RightIcon = styled(AiFillCaretRight)`
-    width: 30px;
-    height: 30px;
-    color:  #005187;
+  width: 30px;
+  height: 30px;
+  color: #005187;
 `;
 
 export const DelIcon = styled(VscTrash)`
-    width: 25px;
-    height: 25px;
-    color:  #6C757D;
-    cursor: pointer;
+  width: 25px;
+  height: 25px;
+  color: #6c757d;
+  cursor: pointer;
 `;
 
-export const EditIcon = styled.img`
-    width: 20px;
-    height: 20px;
-    color:  #6C757D;
-    cursor: pointer;
+export const EditIcon = styled(TbFilePencil)`
+  width: 25px;
+  height: 25px;
+  color: #6c757d;
+  cursor: pointer;
 `;
 
 export const ConfIcon = styled.img`
-    width: 30px;
-    height: 30px;
-    color:  #6C757D;
-    cursor: pointer;
+  width: 30px;
+  height: 30px;
+  color: #6c757d;
+  cursor: pointer;
 `;
